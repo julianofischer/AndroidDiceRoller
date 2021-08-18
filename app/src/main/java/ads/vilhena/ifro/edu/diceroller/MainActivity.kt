6ea9e8c2909a77634e8810e3d82fa49045e505df
Juliano Fischer Naves <julianofischer@gmail.com>
2021-08-18 00:44:37 -0400

    Initial commit

---
+++ app/src/main/java/ads/vilhena/ifro/edu/diceroller/MainActivity.kt
@@ -0,0 +1,45 @@
+package ads.vilhena.ifro.edu.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    //var diceImage: ImageView? = null
+    //or
+    lateinit var diceImage: ImageView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = this.findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun rollDice() {
+        //val resultText: TextView = this.findViewById(R.id.result_textview)
+        //resultText.text = randomInt.toString()
+        //val diceImage : ImageView = findViewById(R.id.dice_image)
+
+        val randomInt = Random().nextInt(6) + 1
+
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
